6f93aa4ccfc2f65723591251c6b0108c39fdf3b1
pradip <PP66490@RMGW63ZLPC0495.ind.zensar.com>
2022-05-09 15:26:21 +0530

    added hello

--- stock-applications/src/main/java/com/zensar/stockapplication/StockApplication.java
+++ stock-applications/src/main/java/com/zensar/stockapplication/StockApplication.java
@@ -19,7 +19,7 @@ public class StockApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		
 		SpringApplication.run(StockApplication.class, args);
-		
+		System.out.println("HELLO");
 		
 		Stock stock = new Stock();
 		stock.getStockId();
